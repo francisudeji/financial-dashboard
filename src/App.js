7e21522bcea51edc5d120d31a2dc8cc3295db00a
francisudeji <francis.udejiofficial@gmail.com>
2020-10-21 12:11:07 +0100

    Design sidebar and added custom classes to tailwind config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from './components/Sidebar';
+import './css/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen w-full flex relative'>
+      <div className='w-240px h-screen relative'>
+        <Sidebar />
+      </div>
+      <main className='bg-light-gray h-screen flex-1 p-40-33px'>
+        <p>hello</p>
+      </main>
     </div>
   );
 }
